bd421e82dbc8df32cc8b3fae74aae39e492f8d66
hector fernandez <hectorfrdezr@gmail.com>
2024-05-27 23:42:00 -0400

    integrador-js

---
+++ main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper-1", {
+    sliderPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    navigation: {
+        nextEl:"swiper-button-next",
+        prevEl:"swiper-button-prev",
+    }
+});
